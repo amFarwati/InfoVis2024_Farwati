1c91b2b87513a08452670e84da50df6b465cf954
arthur farwati <arthur-mustapha.farwati@insa-lyon.fr>
2024-11-14 12:07:12 +0900

    commit

--- W06/task1.js
+++ W06/task1.js
@@ -1,12 +1,14 @@
 d3.csv("https://amfarwati.github.io/InfoVis2024_Farwati/W06/w06_task1.csv")
     .then( data => {
-        data.forEach( d => { d.x = +d.x; d.y = +d.y; });
+        data.forEach( d => { d.population_proper = +d.population_proper; d.population = +d.population; });
 
         var config = {
             parent: '#drawing_region',
-            width: 256,
-            height: 256,
-            margin: {top:10, right:10, bottom:20, left:10}
+            width: 800,
+            height: 800,
+            margin: {top:50, right:50, bottom:60, left:80},
+            format : '.2s',
+            type : 'log'
         };
 
         const scatter_plot = new ScatterPlot( config, data );
@@ -23,7 +25,9 @@ class ScatterPlot {
             parent: config.parent,
             width: config.width || 256,
             height: config.height || 256,
-            margin: config.margin || {top:10, right:10, bottom:10, left:10}
+            margin: config.margin || {top:10, right:10, bottom:10, left:10},
+            format: config.format || ',',
+            type: config.type || 'linear'
         }
         this.data = data;
         this.init();
@@ -42,28 +46,39 @@ class ScatterPlot {
         self.inner_width = self.config.width - self.config.margin.left - self.config.margin.right;
         self.inner_height = self.config.height - self.config.margin.top - self.config.margin.bottom;
 
-        self.xscale = d3.scaleLinear()
-            .range( [0, self.inner_width] );
+        self.xscale = self.config.type === 'log'? 
+            d3.scaleLog().range( [0, self.inner_width] ) : d3.scaleLinear().range( [0, self.inner_width] );
 
-        self.yscale = d3.scaleLinear()
-            .range( [0, self.inner_height] );
+        self.yscale =  self.config.type === 'log'? 
+            d3.scaleLog().range( [self.inner_height, 0] ) : d3.scaleLinear().range( [self.inner_height, 0] );
 
         self.xaxis = d3.axisBottom( self.xscale )
-            .ticks(6);
+            .ticks(3)
+            .tickSize(5)
+            .tickFormat(d3.format(self.config.format))
+            .tickPadding(5);
+
+        self.yaxis = d3.axisLeft( self.yscale )
+            .ticks(5)
+            .tickSize(5)
+            .tickFormat(d3.format(self.config.format))
+            .tickPadding(1);
 
         self.xaxis_group = self.chart.append('g')
             .attr('transform', `translate(0, ${self.inner_height})`);
+
+        self.yaxis_group = self.chart.append('g');
     }
 
     update() {
         let self = this;
 
-        const xmin = d3.min( self.data, d => d.x );
-        const xmax = d3.max( self.data, d => d.x );
+        const xmin = d3.min( self.data, d => d.population_proper );
+        const xmax = d3.max( self.data, d => d.population_proper );
         self.xscale.domain( [xmin, xmax] );
 
-        const ymin = d3.min( self.data, d => d.y );
-        const ymax = d3.max( self.data, d => d.y );
+        const ymin = d3.min( self.data, d => d.population );
+        const ymax = d3.max( self.data, d => d.population );
         self.yscale.domain( [ymin, ymax] );
 
         self.render();
@@ -76,9 +91,22 @@ class ScatterPlot {
             .data(self.data)
             .enter()
             .append("circle")
-            .attr("cx", d => self.xscale( d.x ) )
-            .attr("cy", d => self.yscale( d.y ) )
-            .attr("r", d => d.r );
+            .attr("cx", d => self.xscale( d.population_proper ) )
+            .attr("cy", d => self.yscale( d.population ) )
+            .attr("r", d => 5 )
+            .style("fill", "lightgrey");
+
+        self.chart.selectAll("text")
+            .data(self.data)
+            .enter()
+            .append("text")
+            .attr("x",  d => self.xscale( d.population_proper ) )
+            .attr("y", d => self.yscale( d.population ) )
+            .attr("class", "label")
+            .text(function(d){ return d.city; })
+
+        self.yaxis_group 
+            .call( self.yaxis );
 
         self.xaxis_group
             .call( self.xaxis );
