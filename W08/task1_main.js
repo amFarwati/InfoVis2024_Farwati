3186ae834e04f724146110d42109b935df58000b
arthur farwati <arthur-mustapha.farwati@insa-lyon.fr>
2024-12-17 17:09:35 +0900

    commit

--- W08/task1_main.js
+++ W08/task1_main.js
@@ -5,6 +5,7 @@ class BarChart {
             width: config.width || 256,
             height: config.height || 256,
             margin: config.margin || {top:10, right:10, bottom:10, left:10},
+            orientation: config.orientation || 'horizontal',
             format: config.format || ',',
             type : config.type || 'linear',
         }
@@ -27,62 +28,85 @@ class BarChart {
         self.inner_height = self.config.height - self.config.margin.top - self.config.margin.bottom;
 
         // Initialize axis scales
-        self.xscale = self.config.type === 'log'? 
-            d3.scaleLog()
-            .range([0, self.inner_width])
-            : 
-            d3.scaleLinear()
-            .domain([0, d3.max(self.data, d => d.value)])
-            .range([0, self.inner_width]);
- 
-        self.yscale = d3.scaleBand()
-                        .domain(self.data.map(d => d.label))
-                        .range([0, self.inner_height])
-                        .paddingInner(0.1) 
+        self.valueScale = self.config.type === 'log'? 
+        d3.scaleLog()
+        .range([self.config.orientation === 'horizontal' ? 0 : self.inner_height, self.config.orientation === 'horizontal' ? self.inner_width : 0])
+        : 
+        d3.scaleLinear()
+        .range([self.config.orientation === 'horizontal' ? 0 : self.inner_height, self.config.orientation === 'horizontal' ? self.inner_width : 0]);
+    
+        self.labelScale = d3.scaleBand()
+            .domain(self.data.map(d => d.label))
+            .range([0, self.config.orientation === 'horizontal' ? self.inner_height : self.inner_width])
+            .paddingInner(0.3);
+
+        self.xscale = self.config.orientation === 'horizontal' ? self.valueScale : self.labelScale;
+        self.yscale = self.config.orientation === 'horizontal' ? self.labelScale : self.valueScale;
 
         // Initialize axes
-        self.xaxis = d3.axisBottom( self.xscale )
-            .ticks(5)
-            .tickFormat(d3.format(self.config.format))
-            .tickSizeOuter(0);
+        if (self.config.orientation === 'horizontal') {
+            self.xaxis = d3.axisBottom( self.xscale )
+                .ticks(5)
+                .tickFormat(d3.format(self.config.format))
+                .tickSizeOuter(0);
 
-        self.yaxis = d3.axisLeft( self.yscale )
-            .tickSizeOuter(0); 
+            self.yaxis = d3.axisLeft( self.yscale )
+                .tickSizeOuter(0); 
 
+        }else{
+            self.xaxis = d3.axisBottom( self.xscale )
+                .tickSizeOuter(0);
+
+            self.yaxis = d3.axisLeft( self.yscale )
+                .ticks(5)
+                .tickFormat(d3.format(self.config.format))
+                .tickSizeOuter(0);  
+        }
         // Draw the axis
         self.xaxis_group = self.chart.append('g')
-            .attr('transform', `translate(0, ${self.inner_height})`)
-            .call( self.xaxis );
+        .attr('transform', `translate(0, ${self.inner_height})`)
+        .call( self.xaxis );
 
         self.yaxis_group = self.chart.append('g')
-            .call( self.yaxis );   
-        
+        .call( self.yaxis );   
     }
 
     update() {
         let self = this;
 
-        const min = d3.min( self.data, d => d.value );
-        const max = d3.max( self.data, d => d.value );  
-        self.xscale.domain( [min,max] );
-        //self.yscale.domain( [min, max] );
-
+        if (self.config.orientation === 'horizontal') {
+            self.xscale.domain([1, d3.max(self.data, d => d.value)]);
+        } else {
+            self.yscale.domain([1, d3.max(self.data, d => d.value)]);
+        }
+    
         self.render();
     }
 
     render() {
         let self = this;
 
-        self.chart.selectAll("rect").data(self.data).enter()
-        .append("rect")
-        .attr("x", 0)
-        .attr("y", d => self.yscale(d.label))
-        .attr("width", d => self.xscale(d.value))
-        .attr("height", self.yscale.bandwidth());
+        if (self.config.orientation === 'horizontal') {
+            self.chart.selectAll("rect").data(self.data)
+                .enter()
+                .append("rect")
+                .attr("x", 0)
+                .attr("y", d => self.yscale(d.label))
+                .attr("width", d => self.xscale(d.value))
+                .attr("height", self.yscale.bandwidth());
+        } else {
+            self.chart.selectAll("rect").data(self.data)
+                .enter()
+                .append("rect")
+                .attr("x", d => self.xscale(d.label))
+                .attr("y",  d => self.yscale(d.value))
+                .attr("width", self.xscale.bandwidth())
+                .attr("height", d => self.inner_height - self.yscale(d.value));
+        }
     }
 }
 
-d3.csv("https://amfarwati.github.io/InfoVis2024_Farwati/W04/w04_task2.csv")
+d3.csv("https://amfarwati.github.io/InfoVis2024_Farwati/W08/data.csv")
 .then( (data) => {
         let formated_data = data.map( (d) => {return {label : d.city, value :d.population }});
 
@@ -92,11 +116,25 @@ d3.csv("https://amfarwati.github.io/InfoVis2024_Farwati/W04/w04_task2.csv")
             height: 256,
             margin: {top:50, right:50, bottom:60, left:50},
             format : '.2s',
-            type : 'log'
+            type : 'log',
+            orientation : 'horizontal',
+        };
+
+        let config2 = {
+            parent: '#drawing_region2',
+            width: 500,
+            height: 256,
+            margin: {top:50, right:50, bottom:60, left:50},
+            format : '.2s',
+            type : 'linear',
+            orientation : 'vertical',
         };
 
         const barChart = new BarChart( config, formated_data );
         barChart.update();
+
+        const barChart2 = new BarChart( config2, formated_data );
+        barChart2.update();
 })
 .catch( (error) => {
     window.alert(`ERROR : ${error}`);
